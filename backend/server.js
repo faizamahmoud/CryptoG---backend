036bc1f0887b1434138d1957625470f8f92090c4
faizamahmoud <faiza.mahmoud@email.com>
2022-09-17 15:53:47 -0600

    dependencies imported and server is running

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+const morgan = require('morgan');
+
+const helmet = require('helmet'); // a security best practice - look into middleware options
+const cookieParser = require('cookie-parser'); // auth
+
+
+// middleware
+app.use(morgan('tiny'));
+app.use(helmet())
+app.use(cookieParser());
+app.get('/', function(req, res){
+    // console.log('Cookies: ', req.cookies);
+    res.cookie('name', 'express').send('cookie set'); 
+ });
+ 
+
+ // turn on the server listener
+ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
