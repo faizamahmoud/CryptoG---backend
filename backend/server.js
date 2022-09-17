189f50c02e4c6b554b50af373bd55976dfda9749
faizamahmoud <faiza.mahmoud@email.com>
2022-09-17 16:15:18 -0600

    added req.body middleware

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 const cors = require('cors');
 const morgan = require('morgan');
-
 const helmet = require('helmet'); // a security best practice - look into middleware options
 const cookieParser = require('cookie-parser'); // auth
 
 
 // middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(morgan('tiny'));
-app.use(helmet())
+app.use(helmet());
 app.use(cookieParser());
 app.get('/', function(req, res){
     // console.log('Cookies: ', req.cookies);
